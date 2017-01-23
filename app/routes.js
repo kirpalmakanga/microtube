a6b40548f955058c79944b64118f7fdf2447720f
LNC2501 <makanga.kirpal@gmail.com>
2017-01-23 13:47:22 +0100

    removed unresolved import

--- app/routes.js
+++ app/routes.js
@@ -6,7 +6,6 @@ import React from 'react'
 import { IndexRoute, Route } from 'react-router'
 import App from './components/App'
 import Playlists from './components/playlists/Playlists'
-import NotFound from './components/NotFound'
 
 export default function getRoutes ({ dispatch }) {
   return (

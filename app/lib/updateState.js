f6fa8360f97c9dfe30fe3c59cc4e0a0209f5a329
Marchal Yoan <marchalyoan@gmail.com>
2017-04-30 01:34:16 +0200

    fixed api loading

--- app/lib/updateState.js
+++ app/lib/updateState.js
@@ -1,6 +1,7 @@
 export default function updateState(actions, initialState) {
   return (state = initialState, { type, data }) => {
     const action = actions[type]
+    console.log(type, data)
     return action ? Object.assign({}, state, action(data, state)) : state
   }
 }
--- app/routes.js
+++ app/routes.js
@@ -1,18 +1,39 @@
 import { IndexRoute, Route } from 'react-router'
 
+import { getAllPlaylists, getPlaylistItems, getSubscriptions } from './actions/database'
+
 import App from './components/App.jsx'
 import Playlists from './components/playlists/Playlists.jsx'
 import PlaylistItems from './components/playlists/PlaylistItems.jsx'
 import Subscriptions from './components/subscriptions/Subscriptions.jsx'
 import Channel from './components/Channel.jsx'
 
-export default function getRoutes ({ dispatch }) {
+export default function getRoutes ({ getState, dispatch }) {
+  const { auth } = getState()
+
+  console.log('auth', auth)
+
   return (
     <Route path='/' component={App}>
-      <IndexRoute component={Playlists} />
-       <Route path='/playlist/:id' component={PlaylistItems} onLeave={() => dispatch({ type: 'CLEAR_PLAYLIST_ITEMS' })}/>
-       <Route path='/subscriptions' component={Subscriptions} />
-       <Route path='/channel/:id' component={Channel} onLeave={() => dispatch({ type: 'CLEAR_CHANNEL_VIDEOS' })}/>
+      <IndexRoute
+        component={Playlists}
+        onLeave={() => dispatch({ type: 'CLEAR_PLAYLISTS' })}
+      />
+       <Route
+        path='/playlist/:id'
+        component={PlaylistItems}
+        onLeave={() => dispatch({ type: 'CLEAR_PLAYLIST_ITEMS' })}
+      />
+       <Route
+        path='/subscriptions'
+        component={Subscriptions}
+        onLeave={() => dispatch({ type: 'CLEAR_SUBSCRIPTIONS' })}
+      />
+       <Route
+        path='/channel/:id'
+        component={Channel}
+        onLeave={() => dispatch({ type: 'CLEAR_CHANNEL_VIDEOS' })}
+      />
     </Route>
   )
 }

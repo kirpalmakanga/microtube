b1f6c050bf193c1fb72e144e3dd136c25c2a1b9f
Kirpal Makanga <makanga.kirpal@gmail.com>
2017-09-07 18:30:16 +0200

    refactored refreshAccessToken

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,6 @@
 import { IndexRoute, Route } from 'react-router'
-
-import { getAllPlaylists, getPlaylistItems, getSubscriptions } from './actions/database'
 import { refreshAccessToken } from './actions/auth'
+import { getAllPlaylists, getPlaylistItems, getSubscriptions } from './actions/database'
 
 import App from './components/App.jsx'
 import Playlists from './components/containers/Playlists.jsx'
@@ -9,41 +8,33 @@ import Playlist from './components/containers/Playlist.jsx'
 import Subscriptions from './components/containers/Subscriptions.jsx'
 import Channel from './components/containers/Channel.jsx'
 
-export default function getRoutes ({ getState, dispatch }) {
-   function refreshToken () {
-     const requestToken = () => {
-       const { auth } = getState()
+export default function getRoutes({ getState, dispatch }) {
+  function refreshToken() {
+    const requestToken = async (callback = () => null) => {
+      const { auth } = getState()
 
-       if(!auth.refresh) {
-         return
-       }
+      if (!auth.refresh) {
+        return callback()
+      }
 
-       refreshAccessToken(auth.refresh, token => {
-         if (token) {
-             dispatch({ type: 'OAUTH_REFRESH', data: { token } })
-         }
-       })
-     }
+      const token = await refreshAccessToken(auth.refresh)
 
-     const refreshWatcher = setInterval(requestToken, 3540000)
+      if (token) {
+        dispatch({ type: 'OAUTH_REFRESH', data: { token } })
+      }
+    }
 
-     requestToken()
-   }
+    const refreshWatcher = setInterval(() => requestToken(() => clearInterval(refreshWatcher)), 3540000)
+
+    requestToken()
+  }
 
   return (
     <Route path='/' onEnter={refreshToken} component={App}>
-      <IndexRoute component={Playlists} />
-       <Route
-        path='/playlist/:id'
-        component={Playlist}
-        onLeave={() => dispatch({ type: 'CLEAR_PLAYLIST_ITEMS' })}
-      />
-       <Route path='/subscriptions' component={Subscriptions} />
-       <Route
-        path='/channel/:id'
-        component={Channel}
-        onLeave={() => dispatch({ type: 'CLEAR_CHANNEL_VIDEOS' })}
-      />
+      <IndexRoute component={Playlists}/>
+      <Route path='/playlist/:id' component={Playlist} onLeave={() => dispatch({type: 'CLEAR_PLAYLIST_ITEMS'})}/>
+      <Route path='/subscriptions' component={Subscriptions}/>
+      <Route path='/channel/:id' component={Channel} onLeave={() => dispatch({type: 'CLEAR_CHANNEL_VIDEOS'})}/>
     </Route>
   )
 }

b8608c43d383b6eae8ae3a58560a7388c6ab5f51
Kirpal Makanga <makanga.kirpal@gmail.com>
2017-09-11 23:11:17 +0200

    changed public path

--- server/app.js
+++ server/app.js
@@ -56,7 +56,7 @@ app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, '../public')))
+app.use(express.static(path.join(__dirname, '../public/')))
 
 if (argv.dev) {
   app.use(require('webpack-dev-middleware')(compiler, {

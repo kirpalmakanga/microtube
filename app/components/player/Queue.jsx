f88ccbb30f9cdd185480fe212cd061150d1215b5
Kirpal Makanga <makanga.kirpal@gmail.com>
2017-09-17 19:28:50 +0200

    reordered queue methods

--- app/components/player/Queue.jsx
+++ app/components/player/Queue.jsx
@@ -14,11 +14,9 @@ class Queue extends Component {
     }
   }
 
-  componentWillReceiveProps(props){
-   this.setState({ queue: props.player.queue})
-  }
+  componentWillReceiveProps = ({ player }) => this.setState({ queue: player.queue })
 
-  getPlaceholder() {
+  getPlaceholder = () => {
     let placeholder = this.placeholder
     if(!placeholder) {
       placeholder = document.createElement('div')
@@ -33,18 +31,45 @@ class Queue extends Component {
 
     this.setState({
       dragged: currentTarget,
-      container: currentTarget.parentNode,
       placeholder: this.getPlaceholder()
     })
   }
 
+  dragOver = (e) => {
+    const { target, pageY } = e
+    const { dragged, placeholder } = this.state
+
+    e.preventDefault()
+
+    dragged.classList.add('queue__item--hidden')
+
+    if(target.classList.contains('queue__item--placeholder') || !target.getAttribute('draggable')) {
+      return
+    }
+
+    this.setState({ over: target })
+
+    const { container } = this
+    const relY = pageY - target.offsetTop
+    const lastChildY = pageY - container.lastChild.offsetTop + target.offsetHeight / 2
+
+    if(relY <= lastChildY) {
+      this.setState({ nodePlacement: 'after' })
+      container.insertBefore(placeholder, target.nextElementSibling)
+    } else {
+      this.setState({ nodePlacement: 'before' })
+      container.insertBefore(placeholder, target)
+    }
+  }
+
   dragEnd = () => {
+    const { container } = this
     const { queue, dragged, over, placeholder } = this.state
     const from = Number(dragged.dataset.id)
     let to = Number(over.dataset.id)
 
     dragged.classList.remove('queue__item--hidden')
-    dragged.parentNode.removeChild(placeholder)
+    container.removeChild(placeholder)
 
     if(from < to) {
       to--
@@ -59,69 +84,39 @@ class Queue extends Component {
     this.props.dispatch({ type: 'QUEUE_SET', data: queue })
   }
 
-  dragOver = (e) => {
-    const { target, pageY } = e
-    const { dragged, container, placeholder } = this.state
-
-    let relY
-    let lastChildY
-
-    e.preventDefault()
-
-    dragged.classList.add('queue__item--hidden')
-
-    if(target.classList.contains('queue__item--placeholder') || !target.getAttribute('draggable')) {
-      return
-    }
-
-    this.setState({ over: target })
-
-    relY = pageY - target.offsetTop
-    lastChildY = pageY - container.lastChild.offsetTop + target.offsetHeight / 2
-
-    if(relY <= lastChildY) {
-      this.setState({ nodePlacement: 'after' })
-      container.insertBefore(placeholder, target.nextElementSibling)
+  makeSetActiveItem = (isActive, index) => () => {
+    if(!isActive) {
+      this.props.dispatch({ type: 'QUEUE_SET_ACTIVE_ITEM', data: { index } })
     } else {
-      this.setState({ nodePlacement: 'before' })
-      container.insertBefore(placeholder, target)
+      this.handleClickPlay()
     }
   }
 
-  render({ player, isPlaying, isBuffering, handleClickPlay, dispatch }, { queue }) {
-    const { dragEnd, dragStart, dragOver } = this
+  removeItem = (e) => {
+    e.stopPropagation()
+    this.props.dispatch({ type: 'QUEUE_REMOVE', data: index })
+  }
+
+  render({ player, isPlaying, isBuffering, handleClickPlay }, { queue }) {
+    const { dragEnd, dragStart, dragOver, makeSetActiveItem, removeItem } = this
     const { showQueue } = player
 
     return (
-        <div className={['queue shadow--2dp', showQueue ? 'queue--show' : ''].join(' ')} onDragOver={dragOver}>
-            {queue.length ? queue.map(({ active, title }, index) => {
-              return (
-                <QueueItem
-                  key={index}
-                  id={index}
-                  title={title}
-                  isActive={active}
-                  isBuffering={isBuffering}
-                  isPlaying={isPlaying}
-                  onDragStart={dragStart}
-                  onDragEnd={dragEnd}
-                  onClick={() => {
-                    if(!active) {
-                      dispatch({
-                        type: 'QUEUE_SET_ACTIVE_ITEM',
-                        data: { index }
-                      })
-                    } else {
-                      handleClickPlay()
-                    }
-                  }}
-                  onClickRemove={(e) => {
-                    e.stopPropagation()
-                    dispatch({ type: 'QUEUE_REMOVE', data: index })
-                  }}
-                />
-              )
-            }, this) : null }
+        <div className={['queue shadow--2dp', showQueue ? 'queue--show' : ''].join(' ')} onDragOver={dragOver} ref={el => this.container = el} >
+            {queue.length ? queue.map(({ active, title }, index) => (
+              <QueueItem
+                key={index}
+                id={index}
+                title={title}
+                isActive={active}
+                isBuffering={isBuffering}
+                isPlaying={isPlaying}
+                onDragStart={dragStart}
+                onDragEnd={dragEnd}
+                onClick={makeSetActiveItem(active, index)}
+                onClickRemove={removeItem}
+              />
+            ), this) : null}
         </div>
     )
   }

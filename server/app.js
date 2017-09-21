3de9fc21ca873cdf5a3d5e8885432e24efbaa6c0
Kirpal Makanga <makanga.kirpal@gmail.com>
2017-09-21 14:43:35 +0200

    changed dirname

--- server/app.js
+++ server/app.js
@@ -56,7 +56,7 @@ app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, '../public')))
+app.use(express.static(path.join(process.cwd(), 'public')))
 
 if (process.env.NODE_ENV !== 'production') {
   app.use(require('webpack-dev-middleware')(compiler, {

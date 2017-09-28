7e18754899264d6d4d30c95d7a87662a485b48ae
Kirpal Makanga <makanga.kirpal@gmail.com>
2017-09-28 16:30:52 +0200

    removed useless connect

--- app/components/player/QueueItem.jsx
+++ app/components/player/QueueItem.jsx
@@ -1,42 +1,37 @@
 import { h } from 'preact'
-import { connect } from 'preact-redux'
 
-const QueueItem = ({ id, isActive, isPlaying, isBuffering, title, onClick, onClickRemove, onDragEnd, onDragStart, player }) => {
-  return (
-    <div
-      className={['queue__item', isActive ? 'queue__item--active' : ''].join(' ')}
-      onClick={onClick}
-      onDragStart={onDragStart}
-      onDragEnd={onDragEnd}
-      data-title={title}
-      draggable
-    >
+const QueueItem = ({ id, isActive, isPlaying, isBuffering, title, onClick, onClickRemove, onDragEnd, onDragStart }) => (
+  <div
+    className={['queue__item', isActive ? 'queue__item--active' : ''].join(' ')}
+    onClick={onClick}
+    onDragStart={onDragStart}
+    onDragEnd={onDragEnd}
+    data-title={title}
+    draggable
+  >
 
-      <div className='queue__item-button icon-button'>
-        <span className={['icon', isActive && isBuffering ? 'rotating': ''].join(' ')}>
-          {isActive && isBuffering ? (
-            <svg><use xlinkHref='#icon-loading'></use></svg>
-          )
-          : isActive && isPlaying ? (
-            <svg><use xlinkHref='#icon-pause'></use></svg>
-          ) : (
-            <svg><use xlinkHref='#icon-play'></use></svg>
-          )}
-        </span>
-      </div>
-
-      <button
-        className='queue__item-button icon-button'
-        onClick={onClickRemove}
-      >
-        <span className='icon'>
-          <svg><use xlinkHref='#icon-close'></use></svg>
-        </span>
-      </button>
+    <div className='queue__item-button icon-button'>
+      <span className={['icon', isActive && isBuffering ? 'rotating': ''].join(' ')}>
+        {isActive && isBuffering ? (
+          <svg><use xlinkHref='#icon-loading'></use></svg>
+        )
+        : isActive && isPlaying ? (
+          <svg><use xlinkHref='#icon-pause'></use></svg>
+        ) : (
+          <svg><use xlinkHref='#icon-play'></use></svg>
+        )}
+      </span>
     </div>
-  )
-}
 
-const mapStateToProps = ({ player }) => ({ player })
+    <button
+      className='queue__item-button icon-button'
+      onClick={onClickRemove}
+    >
+      <span className='icon'>
+        <svg><use xlinkHref='#icon-close'></use></svg>
+      </span>
+    </button>
+  </div>
+)
 
-export default connect(mapStateToProps)(QueueItem)
+export default QueueItem

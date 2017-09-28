b6f92334bd8d0c4c8b87d3e7a4a3ccccc94b23eb
Kirpal Makanga <makanga.kirpal@gmail.com>
2017-09-28 19:47:16 +0200

    refactored thumbnail selector

--- app/lib/getThumbnails.js
+++ app/lib/getThumbnails.js
@@ -1,23 +1,3 @@
-export default function getThumbnail(thumbnails, size) {
-  if(size) {
-      return thumbnails[size].url
-  }
-
-  if(thumbnails.highres) {
-    return thumbnails.highres.url
-  }
-
-  if(thumbnails.standard) {
-    return thumbnails.standard.url
-  }
-
-  if(thumbnails.medium) {
-    return thumbnails.medium.url
-  }
-
-  if(thumbnails.high) {
-    return thumbnails.high.url
-  }
-
-  return thumbnails.default.url
+export default function getThumbnail(thumbnails, size = 'default') {
+  return thumbnails[size].url.replace('http:', 'https:')
 }

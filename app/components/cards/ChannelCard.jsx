02e2f83a9fce15117a27713fe4ebc457165a5568
Kirpal Makanga <makanga.kirpal@gmail.com>
2017-12-18 23:59:45 +0100

    moved channel item count

--- app/components/cards/ChannelCard.jsx
+++ app/components/cards/ChannelCard.jsx
@@ -11,11 +11,13 @@ const SubscriptionCard = ({ id, channelId, title, thumbnails, itemCount, dispatc
   return (
     <div className='card shadow--2dp'>
       <Link className='card__content' href={'/channel/' + channelId} aria-label={`Open channel ${title}`}>
-        <Img src={getThumbnails(thumbnails, 'high')} background/>
+        <div class='card__thumb'>
+          <Img src={getThumbnails(thumbnails, 'high')} background/>
+          <span class="card__thumb-badge">{`${itemCount} video${itemCount !== 1 ? 's' : ''}`}</span>
+        </div>
 
         <div className='card__text'>
           <h2 className='card__text-title'>{title}</h2>
-          <p className='card__text-subtitle'>{itemCount + ' Video' + (itemCount !== 1 ? 's' : '')}</p>
         </div>
       </Link>
     </div>

229fcc01cbc5344a9bcb28efc11b1e2b283da215
Kirpal <makanga.kirpal@gmail.com>
2018-11-20 00:02:51 +0100

    updated offline-plugin, fixed env detection in config file

--- poi.config.js
+++ poi.config.js
@@ -2,21 +2,25 @@ const { IgnorePlugin } = require('webpack');
 const OfflinePlugin = require('offline-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  port: 8080,
-  html: {
-    title: 'Microtube'
-  },
-  transformModules: ['rss-parser'],
-  chainWebpack(config) {
-    config.resolve.extensions.add('.tsx');
+    entry: './src/index.js',
+    port: 8080,
+    html: {
+        title: 'Microtube'
+    },
+    transformModules: ['rss-parser'],
+    chainWebpack(config) {
+        config.resolve.extensions.add('.tsx');
 
-    config
-      .plugin('ignore-moment-locales')
-      .use(IgnorePlugin, [/^\.\/locale$/, /moment$/]);
+        config
+            .plugin('ignore-moment-locales')
+            .use(IgnorePlugin, [/^\.\/locale$/, /moment$/]);
 
-    if (process.NODE_ENV === 'production') {
-      config.plugin('offline').use(OfflinePlugin);
+        if (process.env.NODE_ENV === 'production') {
+            config.plugin('offline').use(OfflinePlugin, [
+                {
+                    autoUpdate: true
+                }
+            ]);
+        }
     }
-  }
 };

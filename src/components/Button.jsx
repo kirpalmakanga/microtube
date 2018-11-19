8c49cd1fd8499d8f6aa0dd22ae3715763fb6a83d
Kirpal <makanga.kirpal@gmail.com>
2018-11-19 23:33:02 +0100

    added sign in button to auth page

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,15 +3,15 @@ import React, { PureComponent } from 'react';
 import Icon from './Icon';
 
 class Button extends PureComponent {
-  render() {
-    const { children, icon, type = 'button', title, ...props } = this.props;
+    render() {
+        const { children, icon, type = 'button', title, ...props } = this.props;
 
-    return (
-      <button type={type} aria-label={title} {...props}>
-        {children ? children : icon ? <Icon name={icon} /> : title}
-      </button>
-    );
-  }
+        return (
+            <button type={type} aria-label={title} {...props}>
+                {children ? children : icon ? <Icon name={icon} /> : title}
+            </button>
+        );
+    }
 }
 
 export default Button;
--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,34 +1,48 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import { signIn } from '../api/youtube';
+
 import { parseURLSearchParams } from '../lib/helpers';
 
 import Screen from '../layout/Screen';
 
+import Button from '../components/Button';
+
 class Login extends Component {
-  redirectIfLogged = () => {
-    const {
-      isSignedIn,
-      location: { search },
-      history
-    } = this.props;
+    redirectIfLogged = () => {
+        const {
+            isSignedIn,
+            location: { search },
+            history
+        } = this.props;
 
-    const { redirect = '/' } = parseURLSearchParams(search);
+        const { redirect = '/' } = parseURLSearchParams(search);
 
-    isSignedIn && history.push(redirect);
-  };
+        isSignedIn && history.push(redirect);
+    };
 
-  componentDidUpdate() {
-    this.redirectIfLogged();
-  }
+    componentDidUpdate() {
+        this.redirectIfLogged();
+    }
 
-  componentDidMount() {
-    this.redirectIfLogged();
-  }
+    componentDidMount() {
+        this.redirectIfLogged();
+    }
 
-  render() {
-    return <Screen>Login</Screen>;
-  }
+    render() {
+        return (
+            <Screen>
+                <div className="log-in">
+                    <Button
+                        className="button"
+                        title="Log in"
+                        onClick={signIn}
+                    />
+                </div>
+            </Screen>
+        );
+    }
 }
 
 const mapStateToProps = ({ auth: { isSignedIn } }) => ({ isSignedIn });

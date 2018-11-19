89da8040b380487e290505182db95b1d40907d10
Kirpal <makanga.kirpal@gmail.com>
2018-11-19 23:07:11 +0100

    fixed Img unmounting

--- src/components/Img.jsx
+++ src/components/Img.jsx
@@ -1,60 +1,68 @@
 import React, { Component } from 'react';
 
 class Img extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      isLoading: true
-    };
-  }
-
-  loadImage = (src) =>
-    new Promise((resolve, reject) => {
-      const img = new Image();
-
-      img.onload = () => resolve(img);
-
-      img.onerror = (error) => reject(console.error('Error: ', error));
-
-      img.src = src;
-    });
-
-  componentDidMount = async () => {
-    try {
-      await this.loadImage(this.props.src);
-
-      this.setState({ isLoading: false });
-    } catch (e) {
-      console.error(e);
+        this.state = {
+            isLoading: true
+        };
     }
-  };
 
-  render() {
-    const {
-      props: { src, alt = 'image', background },
-      state: { isLoading }
-    } = this;
+    loadImage = (src) =>
+        new Promise((resolve, reject) => {
+            const img = new Image();
 
-    return (
-      <figure className={['image', isLoading ? 'loading' : ''].join(' ')}>
-        {background ? (
-          <div
-            className='image-background'
-            style={{ backgroundImage: `url(${src})` }}
-          />
-        ) : (
-          <img src={src} alt={alt} />
-        )}
+            img.onload = () => resolve(img);
 
-        <span className='image-loader'>
-          <svg className='rotating'>
-            <use xlinkHref='#icon-loading' />
-          </svg>
-        </span>
-      </figure>
-    );
-  }
+            img.onerror = (error) => reject(console.error('Error: ', error));
+
+            img.src = src;
+        });
+
+    componentDidMount = async () => {
+        try {
+            await this.loadImage(this.props.src);
+
+            if (this.unmounting) {
+                return;
+            }
+
+            this.setState({ isLoading: false });
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
+    componentWillUnmount() {
+        this.unmounting = true;
+    }
+
+    render() {
+        const {
+            props: { src, alt = 'image', background },
+            state: { isLoading }
+        } = this;
+
+        return (
+            <figure className={['image', isLoading ? 'loading' : ''].join(' ')}>
+                {background ? (
+                    <div
+                        className="image-background"
+                        style={{ backgroundImage: `url(${src})` }}
+                    />
+                ) : (
+                    <img src={src} alt={alt} />
+                )}
+
+                <span className="image-loader">
+                    <svg className="rotating">
+                        <use xlinkHref="#icon-loading" />
+                    </svg>
+                </span>
+            </figure>
+        );
+    }
 }
 
 export default Img;

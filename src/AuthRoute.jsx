13ead43c8c72f45a810c969519bd17cc350ed6c6
Kirpal <makanga.kirpal@gmail.com>
2018-12-11 17:32:31 +0100

    fixed auth redirection

--- src/AuthRoute.jsx
+++ src/AuthRoute.jsx
@@ -4,23 +4,24 @@ import { connect } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
 
 class AuthRoute extends Component {
-  render() {
-    const { isSignedIn, component: Component, ...props } = this.props;
-    const { path } = props;
+    render() {
+        const { isSignedIn, component: Component, ...props } = this.props;
 
-    return (
-      <Route
-        {...props}
-        render={(componentProps) =>
-          isSignedIn ? (
-            <Component {...componentProps} />
-          ) : (
-            <Redirect to={`/login?redirect=${path}`} />
-          )
-        }
-      />
-    );
-  }
+        return (
+            <Route
+                {...props}
+                render={(componentProps) =>
+                    isSignedIn ? (
+                        <Component {...componentProps} />
+                    ) : (
+                        <Redirect
+                            to={`/login?redirect=${props.location.pathname}`}
+                        />
+                    )
+                }
+            />
+        );
+    }
 }
 
 const mapStateToProps = ({ auth: { isSignedIn } }) => ({ isSignedIn });

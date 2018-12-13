bfd6aaca040afcb0a4c3a62baf5c0c4c55332f57
Kirpal Makanga <makanga.kirpal@gmail.com>
2018-12-13 10:55:33 +0100

    updated client ID

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 export const API_URL = 'https://apis.google.com/js/api.js';
 
 export const GOOGLE_CLIENT_ID =
-    '440745412600-snpeajuh0l9tqfrt356mec6j3mdn8eoo.apps.googleusercontent.com';
+    '172905821643-blr64u999b9v1vmqd6rovr3qvs03fcda.apps.googleusercontent.com';
 
 export const GOOGLE_CLIENT_SCOPE = 'https://www.googleapis.com/auth/youtube';
 

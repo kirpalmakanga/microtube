e5eff56a7862dec16806b87f09dc1055fad1b4cf
Kirpal <makanga.kirpal@gmail.com>
2019-03-03 15:30:52 +0100

    removed unused global var

--- src/config/api.js
+++ src/config/api.js
@@ -4,5 +4,3 @@ export const GOOGLE_CLIENT_ID =
     '172905821643-blr64u999b9v1vmqd6rovr3qvs03fcda.apps.googleusercontent.com';
 
 export const GOOGLE_CLIENT_SCOPE = 'https://www.googleapis.com/auth/youtube';
-
-export const FEED_URL = 'https://www.youtube.com/feeds/videos.xml';

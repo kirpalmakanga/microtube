549375cd2ac12d08a706577c85df1d73d1b57bd1
Kirpal <makanga.kirpal@gmail.com>
2019-08-02 22:15:08 +0200

    updated AuthRoute

--- src/AuthRoute.jsx
+++ src/AuthRoute.jsx
@@ -1,36 +1,30 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import { Route, Redirect } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 import Loader from './components/Loader';
 import Login from './containers/Login';
 
-class AuthRoute extends Component {
-    render() {
-        const {
-            isSignedIn,
-            isSigningIn,
-            component: Container,
-            ...props
-        } = this.props;
-
-        return (
-            <Route
-                {...props}
-                render={(componentProps) =>
-                    isSignedIn ? (
-                        <Container {...componentProps} />
-                    ) : isSigningIn ? (
-                        <Loader isActive={true} />
-                    ) : (
-                        <Login {...componentProps} />
-                    )
-                }
-            />
-        );
-    }
-}
+const AuthRoute = ({
+    isSignedIn,
+    isSigningIn,
+    component: Container,
+    ...props
+}) => (
+    <Route
+        {...props}
+        render={(componentProps) =>
+            isSignedIn ? (
+                <Container {...componentProps} />
+            ) : isSigningIn ? (
+                <Loader isActive={true} />
+            ) : (
+                <Login {...componentProps} />
+            )
+        }
+    />
+);
 
 const mapStateToProps = ({ auth: { isSignedIn, isSigningIn } }) => ({
     isSignedIn,

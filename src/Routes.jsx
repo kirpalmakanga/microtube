a372a881256b43997c99a9c6893e47ec820e9acb
Kirpal <makanga.kirpal@gmail.com>
2020-01-19 16:30:25 +0100

    updated lazy component generation

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,7 +1,6 @@
-import { lazy } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import WaitingComponent from './components/WaitingComponent';
+import LazyComponent from './components/LazyComponent';
 
 import AuthRoute from './AuthRoute';
 
@@ -9,10 +8,10 @@ import Playlists from './containers/Playlists';
 import Login from './containers/Login';
 import NotFound from './containers/NotFound';
 
-const Playlist = WaitingComponent(lazy(() => import('./containers/Playlist')));
-const Search = WaitingComponent(lazy(() => import('./containers/Search')));
-const Channels = WaitingComponent(lazy(() => import('./containers/Channels')));
-const Channel = WaitingComponent(lazy(() => import('./containers/channel')));
+const Playlist = LazyComponent(() => import('./containers/Playlist'));
+const Search = LazyComponent(() => import('./containers/Search'));
+const Channels = LazyComponent(() => import('./containers/Channels'));
+const Channel = LazyComponent(() => import('./containers/channel'));
 
 const Routes = () => (
     <Switch>

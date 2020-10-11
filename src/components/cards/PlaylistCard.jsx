60c282e072b4a2b68d269a4be981bd6fc6a5e538
Kirpal <makanga.kirpal@gmail.com>
2020-10-11 18:41:01 +0200

    turned cards into memoized functional components

--- src/components/cards/PlaylistCard.jsx
+++ src/components/cards/PlaylistCard.jsx
@@ -1,4 +1,4 @@
-import { PureComponent } from 'react';
+import { memo } from 'react';
 
 import { getThumbnails, preventDefault } from '../../lib/helpers';
 
@@ -10,42 +10,34 @@ import CardButtons from './CardButtons';
 import Button from './CardButton';
 import Title from './CardTitle';
 
-class PlaylistCard extends PureComponent {
-    render() {
-        const {
-            title,
-            thumbnails,
-            itemCount,
-            onClick,
-            onClickMenu
-        } = this.props;
+const PlaylistCard = ({
+    title,
+    thumbnails,
+    itemCount,
+    onClick,
+    onClickMenu
+}) => (
+    <CardContainer onContextMenu={preventDefault(onClickMenu)}>
+        <CardContent onClick={onClick}>
+            <CardThumbnail
+                src={getThumbnails(thumbnails, 'medium')}
+                altText={title}
+                badgeText={`${itemCount} video${itemCount !== 1 ? 's' : ''}`}
+            />
 
-        return (
-            <CardContainer onContextMenu={preventDefault(onClickMenu)}>
-                <CardContent onClick={onClick}>
-                    <CardThumbnail
-                        src={getThumbnails(thumbnails, 'medium')}
-                        altText={title}
-                        badgeText={`${itemCount} video${
-                            itemCount !== 1 ? 's' : ''
-                        }`}
-                    />
+            <CardContentInner>
+                <Title>{title}</Title>
+            </CardContentInner>
+        </CardContent>
 
-                    <CardContentInner>
-                        <Title>{title}</Title>
-                    </CardContentInner>
-                </CardContent>
+        <CardButtons>
+            <Button
+                title="Open playlist menu"
+                icon="more"
+                onClick={onClickMenu}
+            />
+        </CardButtons>
+    </CardContainer>
+);
 
-                <CardButtons>
-                    <Button
-                        title="Open playlist menu"
-                        icon="more"
-                        onClick={onClickMenu}
-                    />
-                </CardButtons>
-            </CardContainer>
-        );
-    }
-}
-
-export default PlaylistCard;
+export default memo(PlaylistCard);
--- src/components/cards/VideoCard.jsx
+++ src/components/cards/VideoCard.jsx
@@ -1,4 +1,4 @@
-import { PureComponent } from 'react';
+import { memo } from 'react';
 import { Link } from 'react-router-dom';
 
 import {
@@ -18,61 +18,51 @@ import Button from './CardButton';
 import Title from './CardTitle';
 import Subtitle from './CardSubtitle';
 
-class VideoCard extends PureComponent {
-    render() {
-        const {
-            title,
-            thumbnails,
-            publishedAt,
-            privacyStatus,
-            duration,
-            channelId,
-            channelTitle,
-            onClick,
-            onClickMenu
-        } = this.props;
+const VideoCard = ({
+    title,
+    thumbnails,
+    publishedAt,
+    privacyStatus,
+    duration,
+    channelId,
+    channelTitle,
+    onClick,
+    onClickMenu
+}) => (
+    <CardContainer onContextMenu={preventDefault(onClickMenu)}>
+        <CardContent onClick={onClick}>
+            <CardThumbnail
+                src={getThumbnails(thumbnails, 'medium')}
+                altText={title}
+                badgeText={duration ? formatTime(duration) : null}
+            />
 
-        return (
-            <CardContainer onContextMenu={preventDefault(onClickMenu)}>
-                <CardContent onClick={onClick}>
-                    <CardThumbnail
-                        src={getThumbnails(thumbnails, 'medium')}
-                        altText={title}
-                        badgeText={duration ? formatTime(duration) : null}
-                    />
+            <CardContentInner>
+                <Title>{title}</Title>
 
-                    <CardContentInner>
-                        <Title>{title}</Title>
+                {privacyStatus !== 'deleted' ? (
+                    <Subtitle className="author">
+                        <Link
+                            to={`/channel/${channelId}`}
+                            onClick={stopPropagation()}
+                        >
+                            {channelTitle}
+                        </Link>
+                    </Subtitle>
+                ) : null}
 
-                        {privacyStatus !== 'deleted' ? (
-                            <Subtitle className="author">
-                                <Link
-                                    to={`/channel/${channelId}`}
-                                    onClick={stopPropagation()}
-                                >
-                                    {channelTitle}
-                                </Link>
-                            </Subtitle>
-                        ) : null}
+                {publishedAt ? (
+                    <Subtitle className="date">
+                        {formatDate(publishedAt, 'MMMM do yyyy')}
+                    </Subtitle>
+                ) : null}
+            </CardContentInner>
+        </CardContent>
 
-                        {publishedAt ? (
-                            <Subtitle className="date">
-                                {formatDate(publishedAt, 'MMMM do yyyy')}
-                            </Subtitle>
-                        ) : null}
-                    </CardContentInner>
-                </CardContent>
+        <CardButtons>
+            <Button title="Open video menu" icon="more" onClick={onClickMenu} />
+        </CardButtons>
+    </CardContainer>
+);
 
-                <CardButtons>
-                    <Button
-                        title="Open video menu"
-                        icon="more"
-                        onClick={onClickMenu}
-                    />
-                </CardButtons>
-            </CardContainer>
-        );
-    }
-}
-
-export default VideoCard;
+export default memo(VideoCard);

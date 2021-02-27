c2eeaafb84a6cfe3a74bb06b84a23b139779ee8c
Kirpal <makanga.kirpal@gmail.com>
2021-02-27 13:26:02 +0100

    added pwa plugin

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 
@@ -7,5 +8,26 @@ export default defineConfig({
     esbuild: {
         jsxInject: `import React from 'react'`,
     },
-    plugins: [reactRefresh()],
+    plugins: [
+        reactRefresh(),
+        VitePWA({
+            manifest: {
+                name: 'MicroTube',
+                short_name: 'MicroTube',
+                theme_color: '#191919',
+                background_color: '#191919',
+                display: 'standalone',
+                Scope: '/',
+                start_url: '/',
+                icons: [
+                    {
+                        src: '/icon-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                ],
+                splash_pages: null,
+            },
+        }),
+    ],
 });

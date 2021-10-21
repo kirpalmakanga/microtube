f8f30bb05772d4a31514a4b748233be778e754e5
Kirpal <makanga.kirpal@gmail.com>
2021-10-21 18:50:46 +0200

    Icon: removed optional class

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -2,12 +2,11 @@ import { Component, Show } from 'solid-js';
 
 interface Props {
     name: string;
-    className?: string;
 }
 
 const Icon: Component<Props> = (props: Props) => (
     <Show when={props.name}>
-        <span className={['icon', props.className].filter(Boolean).join(' ')}>
+        <span className="icon">
             <svg>
                 <use href={`#icon-${props.name}`} />
             </svg>

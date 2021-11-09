8162377f2d90f41320cf4047715bdd46282eea33
Kirpal <makanga.kirpal@gmail.com>
2021-11-09 02:32:33 +0100

    youtube player api: fixed YouTubePlayer type

--- src/api/youtube-player.ts
+++ src/api/youtube-player.ts
@@ -63,125 +63,89 @@ export interface Options {
 }
 
 export interface YouTubePlayer {
-    addEventListener(
+    addEventListener: (
         event: string,
         listener: (event: CustomEvent) => void
-    ): void;
-    destroy(): void;
-    getAvailablePlaybackRates(): ReadonlyArray<number>;
-    getAvailableQualityLevels(): ReadonlyArray<string>;
-    getCurrentTime(): number;
-    getDuration(): number;
-    getIframe(): HTMLIFrameElement;
-    getOption(module: string, option: string): any;
-    getOptions(): string[];
-    getOptions(module: string): object;
-    setOption(module: string, option: string, value: any): void;
-    setOptions(): void;
-    cuePlaylist(
-        playlist: string | ReadonlyArray<string>,
-        index?: number,
-        startSeconds?: number,
-        suggestedQuality?: string
-    ): void;
-    cuePlaylist(playlist: {
+    ) => void;
+    destroy: () => void;
+    getAvailablePlaybackRates: () => ReadonlyArray<number>;
+    getAvailableQualityLevels: () => ReadonlyArray<string>;
+    getCurrentTime: () => number;
+    getDuration: () => number;
+    getIframe: () => HTMLIFrameElement;
+    getOption: (module: string, option: string) => any;
+    getOptions: (module?: string) => object;
+    setOption: (module: string, option: string, value: any) => void;
+    setOptions: () => void;
+    cuePlaylist: (playlist: {
         listType: string;
         list?: string;
         index?: number;
         startSeconds?: number;
         suggestedQuality?: string;
-    }): void;
-    loadPlaylist(
-        playlist: string | ReadonlyArray<string>,
-        index?: number,
-        startSeconds?: number,
-        suggestedQuality?: string
-    ): void;
-    loadPlaylist(playlist: {
+    }) => void;
+    loadPlaylist: (playlist: {
         listType: string;
         list?: string;
         index?: number;
         startSeconds?: number;
         suggestedQuality?: string;
-    }): void;
-    getPlaylist(): ReadonlyArray<string>;
-    getPlaylistIndex(): number;
-    getPlaybackQuality(): string;
-    getPlaybackRate(): number;
-    getPlayerState(): PlayerStates;
-    getVideoEmbedCode(): string;
-    getVideoLoadedFraction(): number;
-    getVideoUrl(): string;
-    getVolume(): number;
-    cueVideoById(
-        videoId: string,
-        startSeconds?: number,
-        suggestedQuality?: string
-    ): void;
-    cueVideoById(video: {
+    }) => void;
+    getPlaylist: () => ReadonlyArray<string>;
+    getPlaylistIndex: () => number;
+    getPlaybackQuality: () => string;
+    getPlaybackRate: () => number;
+    getPlayerState: () => PlayerStates;
+    getVideoEmbedCode: () => string;
+    getVideoLoadedFraction: () => number;
+    getVideoUrl: () => string;
+    getVolume: () => number;
+    cueVideoById: (video: {
         videoId: string;
         startSeconds?: number;
         endSeconds?: number;
         suggestedQuality?: string;
-    }): void;
-    cueVideoByUrl(
-        mediaContentUrl: string,
-        startSeconds?: number,
-        suggestedQuality?: string
-    ): void;
-    cueVideoByUrl(video: {
+    }) => void;
+    cueVideoByUrl: (video: {
         mediaContentUrl: string;
         startSeconds?: number;
         endSeconds?: number;
         suggestedQuality?: string;
-    }): void;
-    loadVideoByUrl(
-        mediaContentUrl: string,
-        startSeconds?: number,
-        suggestedQuality?: string
-    ): void;
-    loadVideoByUrl(video: {
+    }) => void;
+
+    loadVideoByUrl: (video: {
         mediaContentUrl: string;
         startSeconds?: number;
         endSeconds?: number;
         suggestedQuality?: string;
-    }): void;
-    loadVideoById(
-        videoId: string,
-        startSeconds?: number,
-        suggestedQuality?: string
-    ): void;
-    loadVideoById(video: {
+    }) => void;
+    loadVideoById: (video: {
         videoId: string;
         startSeconds?: number;
         endSeconds?: number;
         suggestedQuality?: string;
-    }): void;
-    isMuted(): boolean;
-    mute(): void;
-    nextVideo(): void;
-    pauseVideo(): void;
-    playVideo(): void;
-    playVideoAt(index: number): void;
-    previousVideo(): void;
-    removeEventListener(
+    }) => void;
+    isMuted: () => boolean;
+    mute: () => void;
+    nextVideo: () => void;
+    pauseVideo: () => void;
+    playVideo: () => void;
+    playVideoAt: (index: number) => void;
+    previousVideo: () => void;
+    removeEventListener: (
         event: string,
         listener: (event: CustomEvent) => void
-    ): void;
-    seekTo(seconds: number, allowSeekAhead: boolean): void;
-    setLoop(loopPlaylists: boolean): void;
-    setPlaybackQuality(suggestedQuality: string): void;
-    setPlaybackRate(suggestedRate: number): void;
-    setShuffle(shufflePlaylist: boolean): void;
-    setSize(width: number, height: number): object;
-    setVolume(volume: number): void;
-    stopVideo(): void;
-    unMute(): void;
-    on(
-        eventType: 'stateChange',
-        listener: (event: CustomEvent & { data: number }) => void
-    ): void;
-    on(eventType: EventType, listener: (event: CustomEvent) => void): void;
+    ) => void;
+    seekTo: (seconds: number, allowSeekAhead: boolean) => void;
+    setLoop: (loopPlaylists: boolean) => void;
+    setPlaybackQuality: (suggestedQuality: string) => void;
+    setPlaybackRate: (suggestedRate: number) => void;
+    setShuffle: (shufflePlaylist: boolean) => void;
+    setSize: (width: number, height: number) => object;
+    setVolume: (volume: number) => void;
+    stopVideo: () => void;
+    unMute: () => void;
+    on: (eventType: EventType, listener: (event: CustomEvent) => void) => void;
 }
 
 export const EVENT_NAMES = [

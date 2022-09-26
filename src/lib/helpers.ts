b99fb7dd81e0245b35c9925ebef295df52a6d460
Kirpal <makanga.kirpal@gmail.com>
2022-09-26 13:59:16 +0200

    helpers/wrapURLS: added target blank

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -183,19 +183,19 @@ export function omit<T extends object, K extends keyof T>(
 }
 
 export const wrapURLs = (text: string) => {
-    // http://, https://, ftp://
     const urlPattern =
         /\b(?:https?|ftp):\/\/[a-z0-9-+&@#\/%?=~_|!:,.;]*[a-z0-9-+&@#\/%=~_|]/gim;
 
-    // www. sans http:// or https://
     const pseudoUrlPattern = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
 
-    // Email addresses
     const emailAddressPattern = /[\w.]+@[a-zA-Z_-]+?(?:\.[a-zA-Z]{2,6})+/gim;
 
     return text
-        .replace(urlPattern, '<a href="$&">$&</a>')
-        .replace(pseudoUrlPattern, '$1<a href="http://$2">$2</a>')
+        .replace(urlPattern, '<a href="$&" target="_blank">$&</a>')
+        .replace(
+            pseudoUrlPattern,
+            '$1<a href="http://$2" target="_blank">$2</a>'
+        )
         .replace(emailAddressPattern, '<a href="mailto:$&">$&</a>');
 };
 

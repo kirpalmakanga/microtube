308abe057880b91466cd95d03ddbaa0015863c1a
Kirpal <makanga.kirpal@gmail.com>
2022-10-06 17:46:57 +0200

    Routes: added page transition

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,8 +1,21 @@
-import { Navigate, Outlet, Route, Routes } from 'solid-app-router';
+import { Navigate, Outlet, Route, Routes } from '@solidjs/router';
 import { Component, lazy, Show } from 'solid-js';
+import { Transition } from 'solid-transition-group';
+
 import Login from './containers/Login';
 import Callback from './containers/Callback';
 import NotFound from './containers/NotFound';
+
+import Playlists from './containers/Playlists';
+import Playlist from './containers/Playlist';
+import Video from './containers/Video';
+import Search from './containers/Search';
+import Subscriptions from './containers/Subscriptions';
+import Channel from './containers/Channel';
+import ChannelVideos from './containers/Channel/ChannelVideos';
+import ChannelPlaylists from './containers/Channel/ChannelPlaylists';
+import ChannelAbout from './containers/Channel/ChannelAbout';
+
 import { useStore } from './store';
 
 const Protected = () => {
@@ -15,39 +28,29 @@ const Protected = () => {
     );
 };
 
-const Playlists = lazy(() => import('./containers/Playlists'));
-const Playlist = lazy(() => import('./containers/Playlist'));
-const Video = lazy(() => import('./containers/Video'));
-const Search = lazy(() => import('./containers/Search'));
-const Subscriptions = lazy(() => import('./containers/Subscriptions'));
-const Channel = lazy(() => import('./containers/Channel'));
-const ChannelVideos = lazy(() => import('./containers/Channel/ChannelVideos'));
-const ChannelPlaylists = lazy(
-    () => import('./containers/Channel/ChannelPlaylists')
-);
-
-const ChannelAbout = lazy(() => import('./containers/Channel/ChannelAbout'));
 const Router: Component = () => (
-    <Routes>
-        <Route path="" element={<Protected />}>
-            <Route path="/" element={<Playlists />} />
-            <Route path="/playlist/:playlistId" element={<Playlist />} />
-            <Route path="/video/:videoId" element={<Video />} />
-            <Route path="/search" element={<Search />} />
-            <Route path="/subscriptions" element={<Subscriptions />} />
-            <Route path="/channel/:channelId" element={<Channel />}>
-                <Route path="/" element={<Navigate href="videos" />} />
-                <Route path="/videos" element={<ChannelVideos />} />
-                <Route path="/playlists" element={<ChannelPlaylists />} />
-                <Route path="/about" element={<ChannelAbout />} />
+    <Transition name="fade" mode="outin">
+        <Routes>
+            <Route path="" element={<Protected />}>
+                <Route path="/" element={<Playlists />} />
+                <Route path="/playlist/:playlistId" element={<Playlist />} />
+                <Route path="/video/:videoId" element={<Video />} />
+                <Route path="/search" element={<Search />} />
+                <Route path="/subscriptions" element={<Subscriptions />} />
+                <Route path="/channel/:channelId" element={<Channel />}>
+                    <Route path="/" element={<Navigate href="videos" />} />
+                    <Route path="/videos" element={<ChannelVideos />} />
+                    <Route path="/playlists" element={<ChannelPlaylists />} />
+                    <Route path="/about" element={<ChannelAbout />} />
+                </Route>
             </Route>
-        </Route>
 
-        <Route path="/login" element={<Login />} />
-        <Route path="/callback" element={<Callback />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/callback" element={<Callback />} />
 
-        <Route path="*" element={<NotFound />} />
-    </Routes>
+            <Route path="*" element={<NotFound />} />
+        </Routes>
+    </Transition>
 );
 
 export default Router;

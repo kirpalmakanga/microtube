4164c705c961acfae964a05284c2835094a966ce
Kirpal <makanga.kirpal@gmail.com>
2024-06-24 02:54:04 +0200

    Img: refactoring

--- src/components/Img.tsx
+++ src/components/Img.tsx
@@ -1,4 +1,4 @@
-import { Component, createEffect, Match, Show, Switch } from 'solid-js';
+import { Component, createEffect, Match, onMount, Switch } from 'solid-js';
 import { createStore } from 'solid-js/store';
 import { Transition } from 'solid-transition-group';
 import Icon from './Icon';
@@ -12,33 +12,59 @@ interface Props {
 }
 
 interface State {
-    isLoaded: boolean;
+    isLoading: boolean;
     hasError: boolean;
     img: HTMLImageElement | null;
 }
 
 const Img: Component<Props> = (props) => {
     const [state, setState] = createStore<State>({
-        isLoaded: false,
+        isLoading: true,
         hasError: false,
         img: null
     });
 
+    const loadImage = async () => {
+        try {
+            const { src } = props;
+
+            if (!src) {
+                return;
+            }
+
+            console.log('load !');
+
+            const img = new Image();
+
+            img.src = props.src;
+
+            if (!img.complete) {
+                await new Promise((resolve, reject) => {
+                    img.onload = resolve;
+                    img.onerror = reject;
+                });
+            }
+        } catch (e) {
+            setState({ hasError: true });
+        } finally {
+            console.log('hey');
+            setState({ isLoading: false });
+        }
+    };
+
     createEffect((currentSrc) => {
         const { src } = props;
 
-        if (src && src !== currentSrc) {
-            const img = new Image();
-
-            img.onload = () => setState({ isLoaded: true });
-            img.onerror = () => setState({ hasError: true });
-
-            img.src = src;
-
-            setState('img', img);
+        if (src !== currentSrc) {
+            loadImage();
         }
 
         return src;
+    }, props.src);
+
+    onMount(() => {
+        console.log('mount ?');
+        loadImage();
     });
 
     return (
@@ -49,10 +75,17 @@ const Img: Component<Props> = (props) => {
             <Transition name="fade">
                 <Switch>
                     <Match
-                        when={
-                            props.src && (state.img?.complete || state.isLoaded)
-                        }
+                        when={!props.src || state.isLoading || state.hasError}
                     >
+                        <span class="absolute inset-0 flex items-center justify-center bg-primary-600">
+                            <Icon
+                                class="w-6 h-6 text-primary-100"
+                                name="image"
+                            />
+                        </span>
+                    </Match>
+
+                    <Match when={!state.isLoading}>
                         <img
                             class="block overflow-hidden"
                             classList={{
@@ -61,15 +94,6 @@ const Img: Component<Props> = (props) => {
                             src={props.src}
                             alt={props.alt}
                         />
-                    </Match>
-
-                    <Match when={true}>
-                        <span class="absolute inset-0 flex items-center justify-center bg-primary-600">
-                            <Icon
-                                class="w-6 h-6 text-primary-100"
-                                name="image"
-                            />
-                        </span>
                     </Match>
                 </Switch>
             </Transition>

63b540bbf78a45f4128a8cfbeb487f27cefd2df7
Kirpal Makanga <makanga.kirpal@gmail.com>
2025-07-30 22:24:50 +0200

    QueueHeader: using IconButton for commands

--- src/components/player/QueueHeader.tsx
+++ src/components/player/QueueHeader.tsx
@@ -1,6 +1,5 @@
 import { Component } from 'solid-js';
-import Button from '../Button';
-import Icon from '../Icon';
+import IconButton from '../IconButton';
 
 interface Props {
     itemCount: number;
@@ -20,26 +19,15 @@ const QueueHeader: Component<Props> = (props) => (
         </span>
 
         <nav class="flex px-4 gap-2">
-            <Button
-                class="group relative flex items-center justify-center h-8 w-8 rounded bg-primary-800 hover:bg-primary-700 transition-colors"
-                onClick={props.onClickImport}
-            >
-                <Icon class="text-light-50 w-5 h-5" name="add" />
-            </Button>
+            <IconButton icon="add" onClick={props.onClickImport} />
 
-            <Button
-                class="group relative flex items-center justify-center h-8 w-8 rounded bg-primary-800 hover:bg-primary-700 transition-colors"
-                onClick={props.onClickClear}
-            >
-                <Icon class="text-light-50 w-5 h-5" name="delete" />
-            </Button>
+            <IconButton icon="delete" onClick={props.onClickClear} />
 
-            <Button
-                class="group relative flex items-center justify-center h-8 w-8 rounded bg-primary-800 bg-red-500 hover:bg-red-400 transition-colors"
+            <IconButton
+                class="bg-red-500 hover:bg-red-400"
+                icon="close"
                 onClick={props.onClickClose}
-            >
-                <Icon class="text-light-50 w-5 h-5 " name="close" />
-            </Button>
+            />
         </nav>
     </header>
 );
